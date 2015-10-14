c0662e3941438b171710c9169659ae50ad509e30
Sai Pc <schand31@asu.edu>
2015-10-14 13:43:34 -0700

    Assignment ops

---
+++ Assignments.c
@@ -0,0 +1,17 @@
+//
+// Created by Sai Prashanth Chandramouli on 10/14/15.
+//
+#include <stdio.h>
+
+int main() {
+    setbuf(stdout, NULL);
+    int a = (5, 6, 7);
+    printf("%d\n", a);
+    int b = 5;
+    printf("%d\n", b);
+    int c;
+    c = a, b;
+    printf("%d\n", c);
+    c = (a, b);
+    printf("%d\n", c);
+}
