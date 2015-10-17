5dfa6653752e066f028aa356d2bd6c6530fba103
Sai Pc <schand31@asu.edu>
2015-10-16 23:59:41 -0700

    More pointers

--- Q5pointers.c
+++ Q5pointers.c
@@ -13,16 +13,20 @@ int main() {
     b = (char **) malloc(sizeof(char *)); // memory 1
     c = (char *) malloc(sizeof(char)); // memory 2
 
+    printf("%d\t%d\t%d\t%d\n", a, b, c, d);
+
     d = 'd';
     c = &d;
     b = &c;
     a = &b;
 
-    printf("%s\t%s\t%s\t%s", ***a, **b, *c, d);
+    printf("%c\t%c\t%c\t%c\n", ***a, **b, *c, d);
+    printf("%d\t%d\t%d\t%d\n", a, b, c, d);
 
-    //*a = &c;
-    //***a = d;
+    *a = &c;
+    ***a = d;
     d = 'a';
 
-    //printf("%s\t%s\t%s\t%s", a, b, c, d);
+    printf("%c\t%c\t%c\t%c\n", ***a, **b, *c, d);
+    printf("%d\t%d\t%d\t%d\n", a, b, c, d);
 }
